2512060cac1bcf6062f5f42dc380e850fb1a2ab1
Mesut Mollaoglu <mesutmollaoglu84@gmail.com>
2024-07-13 01:37:37 +0300

    updated gui.h

--- headers/gui.h
+++ headers/gui.h
@@ -8,7 +8,7 @@ namespace GUI
     struct Button
     {
         Sprite image;
-        vec2f position;
+        vec2f pos;
         vec2f size = 1.0f;
         int button;
         Window* window = nullptr;
@@ -22,7 +22,7 @@ namespace GUI
     struct TextButton
     {
         std::string text;
-        vec2f position;
+        vec2f pos;
         vec2f size = 1.0f;
         int button;
         Color textColor = {255, 255, 255, 255};
@@ -37,12 +37,12 @@ namespace GUI
     };
     struct Slider
     {
-        vec2f position;
-        float p = 0.0f;
-        float lineLength = 0.0f;
-        float sliderRadius = 1.0f;
-        Color sliderColor = Color(255, 255, 255, 255);
-        Color lineColor = Color(255, 255, 255, 255);
+        vec2f pos;
+        float slider = 0.0f;
+        float length = 0.0f;
+        float radius = 1.0f;
+        Color sliderColor = {255, 255, 255, 255};
+        Color lineColor = {255, 255, 255, 255};
         Window* window;
         inline Slider() = default;
         inline bool Hover();
@@ -63,23 +63,22 @@ inline GUI::Button::Button(Window* window, const std::string& path, int button) 
 
 inline bool GUI::Button::Clicked()
 {
+    assert(window != nullptr);
     return window->GetMouseButton(button) == Key::Pressed && Hover();
 }
 
 inline bool GUI::Button::Hover()
 {
-    assert(window);
+    assert(window != nullptr);
     const vec2f hs = size * image.GetSize() * 0.5f;
-    const Rect<float> rect = {position - hs, position + hs};
+    const Rect<float> rect = {pos - hs, pos + hs};
     return rect.Contains(window->GetMousePos());
 }
 
 inline void GUI::Button::Draw()
 {
-    assert(window);
-    window->pixelMode = PixelMode::Mask;
-    window->DrawSprite(position.x, position.y, image, size);
-    window->pixelMode = PixelMode::Normal;
+    assert(window != nullptr);
+    window->DrawSprite(pos.x, pos.y, image, size);
 }
 
 inline GUI::TextButton::TextButton(Window* window, const std::string& text, int button) : window(window), button(button), text(text)
@@ -89,41 +88,47 @@ inline GUI::TextButton::TextButton(Window* window, const std::string& text, int 
 
 inline bool GUI::TextButton::Hover()
 {
-    assert(window);
+    assert(window != nullptr);
     const vec2f hs = StringSize(text, size) * 0.5f;
-    const Rect<float> rect = {position - hs, position + hs};
+    const Rect<float> rect = {pos - hs, pos + hs};
     return rect.Contains(window->GetMousePos());
 }
 
 inline bool GUI::TextButton::Clicked()
 {
+    assert(window != nullptr);
     return window->GetMouseButton(button) == Key::Pressed && Hover();
 }
 
 inline void GUI::TextButton::Draw()
 {
-    assert(window);
+    assert(window != nullptr);
     const vec2f size = StringSize(text, size);
-    window->DrawRect(position.x - size.w * 0.5f, position.y - size.h * 0.5f, size.w, size.h, background);
-    window->DrawText(position.x, position.y - size.h * 0.5f, text, size, textColor, 0.5f);
+    window->DrawRect(pos.x - size.w * 0.5f, pos.y - size.h * 0.5f, size.w, size.h, background);
+    window->DrawText(pos.x, pos.y - size.h * 0.5f, text, size, textColor, 0.5f);
 }
 
 inline bool GUI::Slider::Hover()
 {
-    //TODO
-    return false;
+    assert(window != nullptr);
+    return (window->GetMousePos() - vec2f{length * slider, 0.0f} - pos).mag() < radius;
 }
 
 inline void GUI::Slider::Update()
 {
-    //TODO
-    return;
+    assert(window != nullptr);
+    if(Hover() && window->GetMouseButton(0) == Key::Held)
+    {
+        slider = (window->GetMousePos().x - pos.x) / length;
+        slider = std::clamp(slider, 0.0f, 1.0f);
+    }
 }
 
 inline void GUI::Slider::Draw()
 {
-    //TODO
-    return;
+    assert(window != nullptr);
+    window->DrawLine(pos.x, pos.y, pos.x + length, pos.y, lineColor);
+    window->DrawCircle(pos.x + length * slider, pos.y, radius, sliderColor);
 }
 
 #endif
