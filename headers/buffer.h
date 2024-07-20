abbf748f08935e12f4f0083a0abd0a1129a43df8
Mesut Mollaoglu <mesutmollaoglu84@gmail.com>
2024-07-20 23:42:18 +0300

    updated buffer.h

--- headers/buffer.h
+++ headers/buffer.h
@@ -6,27 +6,27 @@
 inline constexpr std::size_t sizeof_enum_type(const GLenum& type)
 {
     switch(type)
-	{
-		case GL_BYTE:
-		case GL_UNSIGNED_BYTE:
-			return sizeof(GLbyte);
-		case GL_SHORT:
-		case GL_UNSIGNED_SHORT:
-			return sizeof(GLshort);
-		case GL_INT_2_10_10_10_REV:
-		case GL_INT:
-		case GL_UNSIGNED_INT_2_10_10_10_REV:
-		case GL_UNSIGNED_INT:
-			return sizeof(GLint);
-		case GL_FLOAT:
-			return sizeof(GLfloat);
-		case GL_DOUBLE:
-			return sizeof(GLdouble);
-		case GL_FIXED:
-			return sizeof(GLfixed);
-		case GL_HALF_FLOAT:
-			return sizeof(GLhalf);
-	}
+    {
+    	case GL_BYTE:
+    	case GL_UNSIGNED_BYTE:
+    		return sizeof(GLbyte);
+    	case GL_SHORT:
+    	case GL_UNSIGNED_SHORT:
+    		return sizeof(GLshort);
+    	case GL_INT_2_10_10_10_REV:
+    	case GL_INT:
+    	case GL_UNSIGNED_INT_2_10_10_10_REV:
+    	case GL_UNSIGNED_INT:
+    		return sizeof(GLint);
+    	case GL_FLOAT:
+    		return sizeof(GLfloat);
+    	case GL_DOUBLE:
+    		return sizeof(GLdouble);
+    	case GL_FIXED:
+    		return sizeof(GLfixed);
+    	case GL_HALF_FLOAT:
+    		return sizeof(GLhalf);
+    }
     return 0;
 }
 
